e31b1d3e3b03c0e115a3f33a5d510674773140c7
DestebanJV <davidji@iadb.org>
2026-03-04 12:35:12 -0500

    Feat: initial init storybook

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+
+
+/** @type { import('@storybook/html-vite').StorybookConfig } */
+const config = {
+  "stories": [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-links"
+  ],
+  "framework": "@storybook/html-vite"
+};
+export default config;
